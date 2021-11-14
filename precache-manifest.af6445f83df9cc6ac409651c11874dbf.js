98653f8b0082a8d580d847a24b921d6b8df8a8e1
bytes123 <minhtan2792001@gmail.com>
2021-11-14 14:59:17 +0700

    Updates

---
+++ precache-manifest.af6445f83df9cc6ac409651c11874dbf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a10be61b7b908a5d0b954cc4114c1da",
+    "url": "/cleverfood/index.html"
+  },
+  {
+    "revision": "8e73e9e0fb36ebf143ad",
+    "url": "/cleverfood/static/css/2.cf04a4dc.chunk.css"
+  },
+  {
+    "revision": "514db0295672425ca675",
+    "url": "/cleverfood/static/css/main.b057fd24.chunk.css"
+  },
+  {
+    "revision": "8e73e9e0fb36ebf143ad",
+    "url": "/cleverfood/static/js/2.5dcc47cd.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/cleverfood/static/js/2.5dcc47cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514db0295672425ca675",
+    "url": "/cleverfood/static/js/main.7ed4525f.chunk.js"
+  },
+  {
+    "revision": "66b3f415ea9e15bc7e68",
+    "url": "/cleverfood/static/js/runtime-main.3fc7e808.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/cleverfood/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/cleverfood/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/cleverfood/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/cleverfood/static/media/slick.f97e3bbf.svg"
+  }
+]);
