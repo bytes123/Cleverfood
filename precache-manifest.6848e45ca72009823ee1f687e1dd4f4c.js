0c6a42aeeb2d0db640d6ddb0b16ce646be13a564
bytes123 <minhtan2792001@gmail.com>
2021-11-14 15:02:14 +0700

    Updates

---
+++ precache-manifest.6848e45ca72009823ee1f687e1dd4f4c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404c94e845c2cc6a56d0c871d118b89d",
+    "url": "/cleverfood/index.html"
+  },
+  {
+    "revision": "30592fcc80d4a29f5d61",
+    "url": "/cleverfood/static/css/2.cf04a4dc.chunk.css"
+  },
+  {
+    "revision": "7ca3edf8958cfb37fa12",
+    "url": "/cleverfood/static/css/main.b057fd24.chunk.css"
+  },
+  {
+    "revision": "30592fcc80d4a29f5d61",
+    "url": "/cleverfood/static/js/2.3eab1e5c.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/cleverfood/static/js/2.3eab1e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca3edf8958cfb37fa12",
+    "url": "/cleverfood/static/js/main.5ba14a77.chunk.js"
+  },
+  {
+    "revision": "66b3f415ea9e15bc7e68",
+    "url": "/cleverfood/static/js/runtime-main.3fc7e808.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/cleverfood/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/cleverfood/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/cleverfood/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/cleverfood/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cleverfood/precache-manifest.af6445f83df9cc6ac409651c11874dbf.js"
+  "/cleverfood/precache-manifest.6848e45ca72009823ee1f687e1dd4f4c.js"
 );
 
 self.addEventListener('message', (event) => {
